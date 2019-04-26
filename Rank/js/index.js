82e15b6ff26eed6d12deb22eced6696b9e6a4182
Ariussssss <1059146811@qq.com>
2019-04-26 20:12:04 +0800

    reset as dev

--- Rank/js/index.js
+++ Rank/js/index.js
@@ -31,37 +31,32 @@ function showList() {
 
 }
 
-// function ajaxFun(method, url, data, flag) {
-//     var dataArr = null;
-//     var xhr = null,
-//         method = method.toUpperCase();
-//     if (window.XMLHttpRequest) {
-//         xhr = new XMLHttpRequest();
-//     } else {
-//         xhr = new ActiveXObject('Microsoft.XMLHttp');
-//     }
-//     if (method == "GET") {
-//         xhr.open(method, url + '?' + data, flag);
-//         xhr.send();
-//     } else if (method == "POST") {
-//         xhr.open(method, url, true);
-//         xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-//         xhr.send(data);
-//     }
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState == 4) {
-//             if (xhr.status == 200) {
-//                 dataArr = JSON.parse(xhr.responseText);
-//                 wtf.create(dataArr);
-//                 page++;
-//             }
-//         }
-//     }
-// }
-
-// mock
 function ajaxFun(method, url, data, flag) {
-    wtf.create(mock.data.illustrations);
+    var dataArr = null;
+    var xhr = null,
+        method = method.toUpperCase();
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject('Microsoft.XMLHttp');
+    }
+    if (method == "GET") {
+        xhr.open(method, url + '?' + data, flag);
+        xhr.send();
+    } else if (method == "POST") {
+        xhr.open(method, url, true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.send(data);
+    }
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                dataArr = JSON.parse(xhr.responseText);
+                wtf.create(dataArr);
+                page++;
+            }
+        }
+    }
 }
 
 function restart() {
